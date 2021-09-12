9c173a55d1c1eca67bf91fb6e20fe12b1c5b095c
Mazzzy <shaikhmazahar@gmail.com>
2021-09-12 17:59:12 +0530

    Phase 1: Added redux flow for selected category item in store

--- shop-app/src/store/actions/categoriesAction.js
+++ shop-app/src/store/actions/categoriesAction.js
@@ -1,5 +1,5 @@
 import { createSelector } from 'reselect';
-import { SET_CATEGORIES } from '../types';
+import { SET_CATEGORIES, SET_SELECTED_CATEGORY } from '../types';
 
 // dispatches to set fetched (via API call) categories inside store
 export const setCategories = (categoriesList) => {
@@ -9,9 +9,25 @@ export const setCategories = (categoriesList) => {
     };
 };
 
-// get available categories list from the store
+// dispatched to set the clicked category as selected one [used to get products by category]
+export const setSelectedCategory = (categoryItem) => {
+    return {
+      type: SET_SELECTED_CATEGORY,
+      payload: categoryItem,
+    };
+  };
+
+// selectors to get categories from store
 const getCategoriesSelector = state => state.categories;
+
+// get available categories list from the store
 export const getAvailableCategoriesSelector = createSelector(
-    getCategoriesSelector,
-    ({ categories }) => (categories)
+  getCategoriesSelector,
+  ({ categories }) => (categories)
+);
+
+// // get selected category from store
+export const getSelectedCategorySelector = createSelector(
+  getCategoriesSelector,
+  ({ selectedCategory }) => (selectedCategory)
 );

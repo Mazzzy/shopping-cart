1d389df1ecb1ed7cd3dfca628eac1a7889ef103f
Mazzzy <shaikhmazahar@gmail.com>
2021-09-12 13:55:50 +0530

    Phase 0: Added hack to avoid process related redux error and added logger utility for redux operations in console.

--- shop-app/src/utils/index.js
+++ shop-app/src/utils/index.js
@@ -1,6 +1,13 @@
 // configurable define custom element call
 export const defineCustomElement = (tagName, element) => {
     if (!customElements.get(tagName)) {
-      customElements.define(tagName, element)
+      customElements.define(tagName, element);
     }
+}
+// logging redux operation in console
+export const storeLogger = store => next => action => {
+  console.log('dispatching', action);
+  const result = next(action);
+  console.log('next state', store.getState());
+  return result;
 }

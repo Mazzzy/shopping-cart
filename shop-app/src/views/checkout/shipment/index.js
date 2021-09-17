1fecb8a0c999a76d61b81f2ed06373e9474ccd18
Mazzzy <shaikhmazahar@gmail.com>
2021-09-17 15:06:58 +0530

    Phase 7: Added custom required validations for shipment details form

--- shop-app/src/views/checkout/shipment/index.js
+++ shop-app/src/views/checkout/shipment/index.js
@@ -19,7 +19,8 @@ export class ShopShipment extends connect(store)(LitElement) {
         enablePayment: { type: Function },
         email: { type: String },
         name: { type: String },
-        address: { type: String }
+        address: { type: String },
+        errors: { type: Array }
       };
     }
 
@@ -28,6 +29,8 @@ export class ShopShipment extends connect(store)(LitElement) {
       this.email = '';
       this.name = '';
       this.address = '';
+
+      this.errors = [];
     }
 
     handleTxtChange = (e) => {
@@ -36,26 +39,77 @@ export class ShopShipment extends connect(store)(LitElement) {
       this[name] = value;
     }
 
-    validateForm({ email, name, address }) {
-      return email && name && address;
-    }
-
     // trigger to enable flag for displaying payment options
     triggerEnablePayment() {
       this.enablePayment(true)
     }
+
+    // submit shipment details via form
+    submit(e) {
+      e.preventDefault();
+      let form = e.target;
+      this.errors = this.checkForErrors(form);
+      if (!this.errors.length) {
+        // if form contents are valid then move to next step
+        this.triggerEnablePayment();
+
+        // store shipment details in store
+        const formData = new FormData(form);
+        const email = formData.get('email');
+        const name = formData.get('name');
+        const address = formData.get('address');
+        store.dispatch(addShipmentInCheckout({ name, email, address }));
+      }
+    }
+
+    // validate form fields
+    checkForErrors(form) {
+      const { email, name, address } = form;
+      let errors = [];
+   
+      if (!name.value) {
+        errors.push('name');
+      }
   
+      if (!email.value) {
+        errors.push('email');
+      }
+  
+      if (!address.value) {
+        errors.push('address');
+      }
+      return errors;
+    }
+
+    // on change check and update errors for form fields
+    formValueUpdated(e) {
+      let errorList = [...this.errors];
+      if (!e.target.value) {
+        errorList.push(e.target.name);
+      } else {
+        let indexOfError = errorList.indexOf(e.target.name);
+        if (indexOfError >= 0) {
+          errorList.splice(indexOfError, 1);
+        }
+      }
+      this.errors = [...errorList];
+    }
+
     render() {
-      const { email, name, address, handleTxtChange, validateForm } = this;
-      
-      return html`
+      const { email, name, address, handleTxtChange } = this;
+      const hasFormError = (name) => (this.errors.indexOf(name) >= 0 ? 'error' : '');
+      return html `
         <h1>Shipment Details:</h1>
         <div class="shipment-container">
-          <form method="POST">
+          <form 
+            @submit="${(e) => this.submit(e)}"
+            @change="${(e) => this.formValueUpdated(e)}"
+            >
             <ul class="form-container">
               <li>
                 <label for="email">Email:</label>
                 <shop-textbox
+                  .hasError=${hasFormError}
                   .type=${"email"}
                   .name=${"email"}
                   .value=${email}
@@ -65,6 +119,7 @@ export class ShopShipment extends connect(store)(LitElement) {
               <li>
                 <label for="name">Name:</label>
                 <shop-textbox
+                  .hasError=${hasFormError}
                   .name=${"name"}
                   .value=${name}
                   .handleChange=${handleTxtChange}
@@ -73,30 +128,19 @@ export class ShopShipment extends connect(store)(LitElement) {
               <li>
                 <label for="address">Address:</label>
                 <shop-textbox
+                  .hasError=${hasFormError}
                   .name=${"address"}
                   .value=${address}
                   .handleChange=${handleTxtChange}
                 />
               </li>
               <li>
-                ${validateForm({ email, name, address }) && (
-                  html `
-                    <shop-button
-                      .type=${"submit"}
-                      .name=${"proceedToPayBtn"}
-                      .className=${"primary"}
-                      .handleClick=${() => {
-                        console.log('Display payment option')
-                        this.triggerEnablePayment();
-
-                        // store shipment details in store
-                        store.dispatch(addShipmentInCheckout({ email, name, address }));
-                      }}
-                    >
-                      Proceed to Pay
-                    </shop-button>
-                  `)
-                }
+              <shop-button
+                .type=${"submit"}
+                .name=${"proceedToPayBtn"}
+                .btnCaption=${"Proceed to pay"}
+                .className=${"primary"}
+              />
               </li>
             </ul>
           </form>
